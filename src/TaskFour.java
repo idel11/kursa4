751901e9b95b5d7ee7c6a1fb188f128205956c06
dmytro_lyshtvan <idel_11@me.com>
2022-04-25 18:47:17 +0200

    added tasks 4 from exam tasks

---
+++ src/TaskFour.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class TaskFour {
+    public static void main(String[] args) {
+
+        System.out.print("Введіть дату: ");
+        Scanner sc = new Scanner(System.in);
+        String date = sc.next();
+
+        String digits = date.replaceAll("[^0-9]", "");
+
+        int sum = 0;
+
+        for (char c : digits.toCharArray()) {
+            int d = Character.getNumericValue(c);
+            sum += d;
+        }
+
+        System.out.println(sum);
+    }
+}
